37a7cbf760310a5c76ab282ea15db8613bc92759
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-24 10:04:50 -0500

    for loop

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
-def myFunction():
-    print('hello world')
 
 
-myFunction()
+def getSum(arr): 
+    total = 0
+    for item in arr: 
+        total = total + item
+    return total
+
+
+print(getSum([1, 2, 3]))
