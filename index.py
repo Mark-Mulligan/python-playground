141f96aeb7be9d2dfdcff27934985fe455c29830
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-26 10:08:40 -0500

    work with dictionaries

--- index.py
+++ index.py
@@ -1,6 +1,19 @@
-def reverse3(nums):
-    nums.reverse()
-    return nums
+def make_dict(str):
+    wordcounts = dict()
+    words = str.split()
+
+    for word in words:
+        lowercase = word.lower()
+        wordcounts[lowercase] = wordcounts.get(lowercase, 0) + 1
+
+    big_word = None
+    big_count = None
+    for word, count in wordcounts.items():
+        if big_count is None or count >= big_count:
+            big_word = word
+            big_count = count
+
+    return (big_word, big_count)
 
 
-print(reverse3([1, 2, 3]))
+print(make_dict('Here is a test to make sure that this test is working.'))
